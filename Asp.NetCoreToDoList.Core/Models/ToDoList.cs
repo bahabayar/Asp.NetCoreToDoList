d5f974bd920eb259bc49f95c51efc349edc8c633
Baha <bahabayar@hotmail.com>
2021-04-22 04:54:38 +0300

    Creating ToDoList Model in Core Layer

---
+++ Asp.NetCoreToDoList.Core/Models/ToDoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.NetCoreToDoList.Core.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
