04de66d87ccbc6f6c319662e298016ed16d5b3d6
Baha <bahabayar@hotmail.com>
2021-04-22 05:25:55 +0300

    Creatintg DbContext class

---
+++ Asp.NetCoreToDoList.Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Asp.NetCoreToDoList.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.NetCoreToDoList.Data
+{
+  public  class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<ToDoList> ToDoLists { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
