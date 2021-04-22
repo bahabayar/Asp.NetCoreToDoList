3cf7ff675501a75148da3ca4f43539a365e1224b
Baha <bahabayar@hotmail.com>
2021-04-22 05:09:12 +0300

    Creating IService interface

---
+++ Asp.NetCoreToDoList.Core/Services/IService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreToDoList.Core.Services
+{
+   public interface IService<TEntity> where TEntity:class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+    }
+}
