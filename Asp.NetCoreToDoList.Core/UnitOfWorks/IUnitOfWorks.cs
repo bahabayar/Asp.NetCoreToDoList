bf83175cb5fb51d23acac0cb052c1a24fc1351b5
Baha <bahabayar@hotmail.com>
2021-04-22 05:18:36 +0300

    Creating IUnitOfWork interface

---
+++ Asp.NetCoreToDoList.Core/UnitOfWorks/IUnitOfWorks.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreToDoList.Core.UnitOfWorks
+{
+   public interface IUnitOfWorks
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
