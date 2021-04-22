f6889012b7feafc73581d7916ada1cd17ccf9f7e
Baha <bahabayar@hotmail.com>
2021-04-22 05:50:56 +0300

    Creating RepositoryClass in Data Layer

---
+++ Asp.NetCoreToDoList.Data/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Asp.NetCoreToDoList.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreToDoList.Data.Repositories
+{
+   
+    class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public readonly DbContext _context;
+        public readonly DbSet<TEntity> _dbset;
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbset = context.Set<TEntity>();
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbset.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbset.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbset.FindAsync(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbset.Remove(entity);
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            
+            _context.Entry(entity).State = EntityState.Modified;
+            return entity;
+        }
+    }
+}
