6082743cb81e4e90f7b57d2f386dc6991deae91c
Baha <bahabayar@hotmail.com>
2021-04-22 05:04:24 +0300

    Creating Generic interface IRepository

---
+++ Asp.NetCoreToDoList.Core/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreToDoList.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+
+    }
+}
