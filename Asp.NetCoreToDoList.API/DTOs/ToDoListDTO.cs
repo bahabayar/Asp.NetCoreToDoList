9f7061a834d3df3329bb4b9756fde96dfa1c477c
Baha <bahabayar@hotmail.com>
2021-04-24 04:35:28 +0300

    Creating ToDoListDTO

---
+++ Asp.NetCoreToDoList.API/DTOs/ToDoListDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreToDoList.API.DTOs
+{
+    public class ToDoListDTO
+    {
+        [Required]
+        public string  TaskName { get; set; }
+        public string TaskDescription { get; set; }
+    }
+}
