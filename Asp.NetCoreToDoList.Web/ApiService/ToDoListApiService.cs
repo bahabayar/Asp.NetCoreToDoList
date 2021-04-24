1403bb0e1100ff9f0efb372ba8842d90a3db9d0b
Baha <bahabayar@hotmail.com>
2021-04-24 19:19:55 +0300

    Writing methods for ToDoListApiService

--- Asp.NetCoreToDoList.Web/ApiService/ToDoListApiService.cs
+++ Asp.NetCoreToDoList.Web/ApiService/ToDoListApiService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Asp.NetCoreToDoList.Web.ApiService
@@ -29,6 +30,59 @@ namespace Asp.NetCoreToDoList.Web.ApiService
             }
             return toDoListDTOs;
 
+        }
+        public async Task<ToDoListDTO> AddAsync(ToDoListDTO toDoListDTO)
+        {
+            var stringContent = new StringContent(JsonConvert.SerializeObject(toDoListDTO), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("todolist",stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                toDoListDTO = JsonConvert.DeserializeObject<ToDoListDTO>(await response.Content.ReadAsStringAsync());
+                return toDoListDTO;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public async Task<ToDoListDTO> GetByIdAsync(int id)
+        {
+            var response = await _httpClient.GetAsync($"todolist/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<ToDoListDTO>(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public async Task<bool> Update(ToDoListDTO toDoListDTO)
+        {
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(toDoListDTO), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync("todolist", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public async Task<bool> Remove(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"todolist/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
